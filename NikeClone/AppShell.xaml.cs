aa4c4f0aa321409aad56c47993a58b4912cef798
Raj <sayyadraj84@gmail.com>
2025-02-05 11:51:17 +0530

    Add project files.

---
+++ NikeClone/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NikeClone
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NikeClone/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NikeClone
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
